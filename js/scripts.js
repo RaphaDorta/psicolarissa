8551030042947dc4bff2a41bd9c16991e008c84c
Raphael Dorta <raphael_dorta@hotmail.com>
2024-05-09 15:51:24 -0300

    PTI Web Standarts

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const questions = document.querySelectorAll('.question');
-    questions.forEach(question => {
-        question.addEventListener('click', function() {
-            const answer = this.nextElementSibling;
-            answer.classList.toggle('active');
-        });
-    });
-});
